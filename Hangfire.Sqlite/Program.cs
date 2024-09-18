6e7e96793d65a10c0f3658f9914d0b5d84bf7a5b
Christian Tricarico <christiantric@hotmail.it>
2024-09-18 12:29:57 +0200

    Add project files.

---
+++ Hangfire.Sqlite/Program.cs
@@ -0,0 +1,42 @@
+using Hangfire;
+using Hangfire.Sqlite.Services;
+using Hangfire.Storage.SQLite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHangfire(config =>
+{
+    config.UseSQLiteStorage("database/hangfire.db", new SQLiteStorageOptions
+    {
+        QueuePollInterval = TimeSpan.FromSeconds(5)
+    });
+});
+
+builder.Services.AddHangfireServer();
+
+builder.Services.AddScoped<IJobService, JobService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHangfireDashboard();
+app.UseHttpsRedirection();
+
+app.MapGet("/job1", () =>
+{
+    var jobId = BackgroundJob.Enqueue<IJobService>((service) => service.ExecuteJob1());
+    return $"Job {jobId} has been queued!";
+});
+
+app.MapGet("/job2", () =>
+{
+    var jobId = BackgroundJob.Enqueue<IJobService>((service) => service.ExecuteJob2());
+    return $"Job {jobId} has been queued!";
+});
+
+var recurringJobManager = app.Services.GetRequiredService<IRecurringJobManager>();
+recurringJobManager.AddOrUpdate<IJobService>("job3", (service) => service.ExecuteJob3(), Cron.Minutely);
+
+app.Run();
---
+++ Hangfire.Sqlite/Services/JobService.cs
@@ -0,0 +1,42 @@
+﻿namespace Hangfire.Sqlite.Services;
+
+public interface IJobService
+{
+    [AutomaticRetry(Attempts = 0)]
+    Task ExecuteJob1();
+
+    Task ExecuteJob2();
+    Task ExecuteJob3();
+}
+
+public class JobService : IJobService
+{
+    private readonly ILogger<JobService> _logger;
+
+    public JobService(ILogger<JobService> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task ExecuteJob1()
+    {
+        _logger.LogInformation("Job 1 started ...");
+        await Task.Delay(5000);
+        throw new Exception("Job 1 failed!");
+        _logger.LogInformation("Job 1 finished!");
+    }
+
+    public async Task ExecuteJob2()
+    {
+        _logger.LogInformation("Job 2 started ...");
+        await Task.Delay(5000);
+        _logger.LogInformation("Job 2 finished!");
+    }
+
+    public async Task ExecuteJob3()
+    {
+        _logger.LogInformation("Job 3 started ...");
+        await Task.Delay(5000);
+        _logger.LogInformation("Job 3 finished!");
+    }
+}
